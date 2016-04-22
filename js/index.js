a3bee857d9e3b66123821c503ddc3bc36e1765fa
Julien Brundrett <julien.brundret@gmail.com>
2016-04-22 02:36:25 -0700

    i suck at git rn

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(".day2-box").hover(function() {
+	$("#day2-tooltip").css("opacity","1");
+}, function() {
+	$("#day2-tooltip").css("opacity","0");
+});
+
+$(".day3-box").hover(function() {
+	$("#day3-tooltip").css("opacity","1");
+}, function() {
+	$("#day3-tooltip").css("opacity","0");
+});
+
+$(".after-box").hover(function() {
+	$("#after-tooltip").css("opacity","1");
+}, function() {
+	$("#after-tooltip").css("opacity","0");
+});
